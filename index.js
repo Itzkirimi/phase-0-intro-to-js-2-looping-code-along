ed34cf8d77634ca6741814bcb01e55bbff3b70eb
Itzkirimi <kirimihansel@gmail.com>
2024-01-15 00:01:54 +0300

    Done with Assignments

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(startingNumber) {
+    let count = startingNumber;
+  
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
